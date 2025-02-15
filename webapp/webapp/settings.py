93dab5dc1deb14a991944d2a9bed2ce7a3823c19
Diego Arana <diegoarana@MacBook-Pro-158.local>
2025-02-15 13:39:23 -0700

    Moved SECRET_KEY to .env file

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -11,6 +11,13 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # Load environment variables from .env
+
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "fallback-key-if-not-set")
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s@!yl^a702zulwq2#+8s35!hp8k3j9&85=j1=&8m#yc3+-141!'
+SECRET_KEY =  
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
