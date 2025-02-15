db9070824362fc03549bac4f253c2cf8284cfcc5
Diego Arana <diegoarana@MacBook-Pro-158.local>
2025-02-15 14:01:54 -0700

    starting again

--- webapp/livebusdata/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Bus
-
-@admin.register(Bus)
-class BusAdmin(admin.ModelAdmin):
-    list_display = ('bus_id', 'route', 'passengers', 'timestamp')
-    search_fields = ('bus_id', 'route')
-
