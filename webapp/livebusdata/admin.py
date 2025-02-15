e253a56df641c8b7bc765c54c1334465e6eab940
Diego Arana <diegoarana@MacBook-Pro-158.local>
2025-02-15 12:50:13 -0700

    [IMPROVEMENT] added django app

---
+++ webapp/livebusdata/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Bus
+
+@admin.register(Bus)
+class BusAdmin(admin.ModelAdmin):
+    list_display = ('bus_id', 'route', 'passengers', 'timestamp')
+    search_fields = ('bus_id', 'route')
+
