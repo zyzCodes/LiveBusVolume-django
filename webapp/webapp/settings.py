56eb5b52c1d76623a455dd2c8d0fb72506528b85
Diego Arana <diegoarana@MacBook-Pro-158.local>
2025-02-15 13:45:35 -0700

    test

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -15,10 +15,9 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()  # Load environment variables from .env
-
-
-
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "fallback-key-if-not-set")
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
