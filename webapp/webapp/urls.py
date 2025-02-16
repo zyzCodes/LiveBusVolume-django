3edfcab51242912aeacdccfc214115be3229d275
Diego Arana <diegoarana@MacBook-Pro-158.local>
2025-02-16 11:58:52 -0700

    mend

--- webapp/webapp/urls.py
+++ webapp/webapp/urls.py
@@ -3,9 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-<<<<<<< HEAD
     path('livebusdata/', include('livebusdata.urls')),  # Include livebusdata routes
-=======
-    path('livebus/', include('livebus.urls')),
->>>>>>> main
 ]
